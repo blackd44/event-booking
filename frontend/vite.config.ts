906b6b9a9f743faf77d21024bf33e4cbceebca6e
blackd44 <irabd44@gmail.com>
2025-07-20 13:46:09 +0200

    add tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import Fonts from "unplugin-fonts/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [
+    react(),
+    Fonts({
+      google: { families: [{ name: "Inter", styles: "wght@400;500;700" }] },
+    }),
+  ],
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 });
